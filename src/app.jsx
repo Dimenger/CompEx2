047e05bfc207adcb222fcc788846a855c1f3268b
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-14 20:10:27 +0400

    Create a list of steps and onClick

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import data from "./data.json";
 export const App = () => {
   // Можно задать 2 состояния — steps и activeIndex
   const [steps, setSteps] = useState(data);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(null);
 
   // И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 
@@ -21,29 +21,22 @@ export const App = () => {
             Контент соответственный шагу. Сейчас активен шаг 3
           </div>
           <ul className={styles["steps-list"]}>
-            {/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
-            <li className={styles["steps-item"] + " " + styles.done}>
-              {/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
-              <button className={styles["steps-item-button"]}>1</button>
-              {/* При клике на кнопку установка выбранного шага в качестве активного */}
-              Шаг 1
-            </li>
-            <li className={styles["steps-item"] + " " + styles.done}>
-              <button className={styles["steps-item-button"]}>2</button>
-              Шаг 2
-            </li>
-            <li
-              className={
-                styles["steps-item"] + " " + styles.done + " " + styles.active
-              }
-            >
-              <button className={styles["steps-item-button"]}>3</button>
-              Шаг 3
-            </li>
-            <li className={styles["steps-item"]}>
-              <button className={styles["steps-item-button"]}>4</button>
-              Шаг 4
-            </li>
+            {steps.map((step) => (
+              <li
+                key={step.id}
+                className={`${styles["steps-item"]} ${styles.done} ${
+                  activeIndex === step.id ? styles.active : ""
+                }`}
+              >
+                <button
+                  onClick={() => setActiveIndex(step.id)}
+                  className={styles["steps-item-button"]}
+                >
+                  {Number(step.id)}
+                </button>
+                {step.title}
+              </li>
+            ))}
           </ul>
           <div className={styles["buttons-container"]}>
             <button className={styles.button}>Назад</button>
