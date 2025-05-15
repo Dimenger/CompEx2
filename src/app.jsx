1c0cd2b50f8790b4b2589b6c090d672d853146ab
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-15 12:47:42 +0400

    Implemented click handlers for Back and Forward buttons.

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,25 @@ import data from "./data.json";
 
 export const App = () => {
   const [steps] = useState(data); // получаем данные  из ./data.json для формирования списка шагов
-  const [activeIndex, setActiveIndex] = useState(null); // устанавливаем активное состояния для выбраного шага
+  const [activeIndex, setActiveIndex] = useState("001"); // устанавливаем активное состояния для выбраного шага
 
   // И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+  const forward = () => {
+    if (!isLastStep) {
+      let nextIndex = Number(activeIndex) + 1;
+      setActiveIndex(nextIndex.toString().padStart(3, "0"));
+    }
+  };
+  const back = () => {
+    if (!isFirstStep) {
+      let previousIndex = Number(activeIndex) - 1;
+      setActiveIndex(previousIndex.toString().padStart(3, "0"));
+    }
+  };
 
   // И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+  const isFirstStep = activeIndex === steps[0].id;
+  const isLastStep = activeIndex === steps.at(-1).id;
 
   return (
     <div className={styles.container}>
@@ -37,8 +51,10 @@ export const App = () => {
             ))}
           </ul>
           <div className={styles["buttons-container"]}>
-            <button className={styles.button}>Назад</button>
-            <button className={styles.button}>
+            <button onClick={back} className={styles.button}>
+              Назад
+            </button>
+            <button onClick={forward} className={styles.button}>
               Далее
               {/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
               {/* Или заменять всю кнопку в зависимости от условия */}
